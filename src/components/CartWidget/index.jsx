efae4f1206157205942d11f6b13f3556b2ac15e6
Juan Lima <juan_jpl@hotmail.com>
2023-07-21 13:34:05 -0300

    update v1.0.3

---
+++ src/components/CartWidget/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+
+
+function CartWidget(){
+    return(
+        <div>
+            <h2>carrito compras</h2>
+        </div>
+    )
+}
+
+export default CartWidget;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+import CartWidget from "../CartWidget";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="brand">
+        <h1>Bravo-Clothing</h1>
+      </div>
+      <div className="botonera">
+        <button>HOME</button>
+        <button>SHOP</button>
+        <button>BLOG</button>
+        <button>PAGES</button>
+        <button>ABOUT US</button>
+        <button>CONTACT</button>
+      </div>
+      <div>
+        <CartWidget/>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

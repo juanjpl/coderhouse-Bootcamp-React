6e4b4f35dfc46202e472ff49ea569ed8c70204c6
Juan Lima <juan_jpl@hotmail.com>
2023-07-29 20:17:50 -0300

    update v1.0.3

---
+++ components/ItemCounter/index.jsx
@@ -0,0 +1,31 @@
+import './styles.css';
+import { useState , useEffect } from 'react';
+
+function ItemCounter({stock, onAdd}){
+
+    const [count, setCount] = useState(1);
+
+    useEffect(()=>{
+      console.log(`Productos a agregar ${count} `)
+      },[count])
+
+    return (
+      <>
+        <div className="card">
+            <p>El stock del producto es: {stock} </p>
+          <p>{count} </p>
+          <button disabled={count >= stock} onClick={() => setCount((count) => count + 1)}>
+           Increment
+          </button>
+          <button disabled={count <= 1} onClick={() => setCount((count) => count - 1)}>
+           Decrement
+          </button>
+          <button  disabled={stock<=0} onClick={() => onAdd(count)}>
+           Agregar al carrito
+          </button>
+        </div>
+      </>
+    )
+}
+
+export default ItemCounter;

3ecba66f49c4d2475a23b9ac1d7c79f5459f966e
Juan Lima <juan_jpl@hotmail.com>
2023-07-29 22:20:57 -0300

    update v1.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useRef , useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [timerDays, setTimerDays] = useState('10');
+const [timerHours, setTimerHours] = useState('00');
+const [timerMinutes, setTimerMinutes] = useState('00');
+const [timerSeconds, setTimerSeconds] = useState('00');
+
+let interval = useRef();
+
+const startTimer =()=>{
+  const countdownDate = new Date('Jan 1, 2023 00:00:00').getTime();
+
+  interval.current = setInterval(()=>{
+    const now = new Date().getTime();
+    const distance= now -countdownDate ;
+   
+
+    const days =Math.floor(  distance/(1000*60*60*24));
+    const hours =Math.floor(  (distance%(1000*60*60*24))/(1000*60*60));
+    const minutes =Math.floor(  (distance%(1000*60*60))/(1000*60));
+    const seconds =Math.floor(  (distance%(1000*60))/1000);
+   
+
+    if(distance <0){
+      //stop ou time
+      clearInterval(interval.current);
+        }else{
+          //update timer
+          setTimerDays(days);
+          setTimerHours(hours);
+          setTimerMinutes(minutes);
+          setTimerSeconds(seconds);
+        }
+      
+      
+  },1000);
+};
+
+
+//componentDidMount
+useEffect(()=>{
+  startTimer();
+  return()=>{
+    clearInterval(interval.current);
+  }
+})
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+     
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="section">
+        <p  className='hour'>{timerDays}</p>
+       <p>days</p>
+        </div>
+        <p>:</p>
+        <div className="section">
+        <p  className='hour'>{timerHours}</p>
+       <p>hours</p>
+        </div>
+        <p>:</p>
+        <div className="section">
+        <p className='hour'>{timerMinutes}</p>
+       <p>minutos</p>
+        </div>
+        <p>:</p>
+        <div className="section">
+        <p  className='hour'>{timerSeconds}</p>
+       <p>seconds</p>
+        </div>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+     
     </>
   )
 }
 
-export default App
+
+export default App;

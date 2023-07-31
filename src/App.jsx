082d82d61a2ba7f5e0af67967cbd9cdb55445619
Juan Lima <juan_jpl@hotmail.com>
2023-07-31 14:31:49 -0300

    update v1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,78 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [loading , setLoading] = useState(false);
+  const [pokemons , setPokemons] = useState([]);
+
+  const productos =[
+    {id:'1', name:'Zapatos Adidas', cantidad:5},
+    {id:'2', name:'Zapatos Nike', cantidad:3},
+    {id:'3', name:'Zapatos Sketch', cantidad:10}
+  ]
+
+  //https://pokeapi.co/api/v2/pokemon/ditto
+
+  //La forma antigua
+  /*
+  const resultado = new Promise((resolve,reject)=>{
+    console.log('Cargando productos...')
+    reject('Error en la promesa')
+    setTimeout(()=>resolve(productos),3000)
+  });
+
+  resultado
+  .then((response)=>console.log(response))
+  .catch((error)=>console.log(error))
+console.log(typeof resultado)
+
+
+//usando sugar syntax
+//Promise utilizando el async/await
+
+async function mostrarProductos(){
+  return await setTimeout(()=>console.log(productos),3000);
+}
+
+console.log(mostrarProductos())
+*/
+
+async function mostrarPokemon(){
+  setLoading(true);
+  const result = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+
+  return result;
+
+}
+
+useEffect(()=>{
+  mostrarPokemon()
+  .then((response)=>response.json())
+  .then((data)=>setPokemons(data.results))
+  .catch((error)=>console.log(error))
+  .finally(()=>setLoading(false))
+},[])
+
+
+const resultado= productos.map((element,index)=>{
+  if(element.id==='2'){
+    return{...element,cantidad:0}
+  }else{
+    return element
+  }
+})
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <>     
       <div className="card">
+        {loading?'cargando...':pokemons.map(((pokemon,index )=><li key={index} >{pokemon.name} </li>))}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
